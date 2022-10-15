cc99fd97e286277d455cad864221f94be53e5516
Abrar <abrarfahimr@gmail.com>
2022-10-15 17:02:26 -0400

    add navigation for tablet in header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,8 @@
 import './Header.scss';
 import logo from '../../assets/icons/logo.svg';
 import nav from '../../assets/icons/nav.svg';
+import code from '../../assets/icons/code.svg';
+import user from '../../assets/icons/user.svg';
 import { NavLink } from 'react-router-dom';
 
 const Header = ({ toggleSideBar, sidebar }) => {
@@ -11,17 +13,42 @@ const Header = ({ toggleSideBar, sidebar }) => {
         className={sidebar ? 'backdrop--open' : 'backdrop'}
         onClick={toggleSideBar}
       ></div>
-      <div className="header">
+      <nav className="header">
         <NavLink to="/">
           <img src={logo} alt="website logo" className="header__logo" />
         </NavLink>
-        <img
-          src={nav}
-          alt="website logo"
-          className="header__nav"
-          onClick={toggleSideBar}
-        />
-      </div>
+        <div className="header__links">
+          <div className="header__link">
+            <button className="header__resume">Resume</button>
+          </div>
+
+          <div className="header__link">
+            <img src={code} alt="work icon" className="header__icon" />
+            <a
+              //update link when later
+              href="/"
+              className="header__anchor"
+            >
+              Work
+            </a>
+          </div>
+
+          <div className="header__link">
+            <img src={user} alt="user icon" className="header__icon" />
+            <NavLink to="/about" className="header__anchor">
+              About Me
+            </NavLink>
+          </div>
+
+          <img
+            src={nav}
+            alt="website logo"
+            className="header__sidebar"
+            onClick={toggleSideBar}
+          />
+          
+        </div>
+      </nav>
     </>
   );
 };
