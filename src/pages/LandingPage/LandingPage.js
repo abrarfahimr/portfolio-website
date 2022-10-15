14b3d4256f7d98d55da362e3689df080b08b739b
Abrar <abrarfahimr@gmail.com>
2022-10-15 18:12:41 -0400

    created landing section and animate hand

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -1,11 +1,29 @@
 import './LandingPage.scss';
+import memoji from "../../assets/images/memoji.png";
+import hand from '../../assets/images/hand.gif';
 
 const LandingPage = () => {
   return (
-    <div className='landing'>
-      
+    <div className="landing">
+      <div className="landing__container">
+        <div className='landing__headercontainer'>
+          <h1 className="landing__header">
+            Hi! <span className="landing__animate">👋</span> This is Abrar,
+          </h1>
+        </div>
+        <h2 className="landing__subheader">
+          A Toronto-based Full Stack Developer bringing interactive,
+          user-friendly, and scalable digital experiences to you.
+        </h2>
+        <p className="landing__focus">
+          Focus: Web Development, Frontend, Backend, UX/UI
+        </p>
+      </div>
+      <div className="landing__imagecontainer">
+        <img src={memoji} alt="memoji" className="landing__image" />
+      </div>
     </div>
-  )
+  );
 }
 
 export default LandingPage;
